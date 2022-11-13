704bbba43fc0172a032c9bd3438a6fd396d6cea5
Diego Ramírez <diego42531@gmail.com>
2022-11-13 11:17:41 -0600

    Register API connected :fu:

--- src/App.jsx
+++ src/App.jsx
@@ -25,6 +25,7 @@ const App = () => {
       <BrowserRouter>
         <Routes>
           <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<SignUp />} />
           <Route path='*' element={<Navigate to='/login' />} />
         </Routes>
       </BrowserRouter>
--- src/services/auth.js
+++ src/services/auth.js
@@ -1,11 +1,29 @@
 import api from "../utils/api"
 
-export const login = async (email, password) => {
+const login = async (email, password) => {
   const response = await api.post('/usuario/login', {email, password})
-  return response
+  return response.data
 }
 
-export const getUsers = async () => {
+const register = async (user) => {
+  const response = await api.post('/usuario', {
+    nombre: user.name,
+    apellidos: user.lastName,
+    email: user.email,
+    password: user.password,
+  })
+  return response.data
+}
+
+const getUsers = async () => {
   const response = await api.get('/usuario')
   return response
 }
+
+const AuthServices = {
+  login,
+  register,
+  getUsers
+}
+
+export default AuthServices
